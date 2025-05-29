0a9276956d78f313fa6ce1bbef9686a288616492
Windmill10 <eason200506@gmail.com>
2025-05-29 08:46:28 +0800

    (feat) added basic gamemanager structure

---
+++ assets/scripts/types/DataTypes.ts
@@ -0,0 +1,5 @@
+export interface PlayerData {
+    actorNumber: number; // Unique identifier for the player
+    name: string; // Player's name
+    avatar: cc.Sprite; // Path to the player's avatar image
+}
