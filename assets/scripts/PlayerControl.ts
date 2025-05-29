f21268af14cfac02a185232c655611e2f01c8200
Joanne <joanne.zh2015@gmail.com>
2025-05-30 01:15:14 +0800

    Player: add function to get PlayerInfo from GameManager

--- assets/scripts/PlayerControl.ts
+++ assets/scripts/PlayerControl.ts
@@ -1,32 +1,34 @@
 const {ccclass, property} = cc._decorator;
+import { PlayerData } from "./types/DataTypes";
+import { MapNodeEvents } from "./types/GameEvents";
+import GameManager from "./GameManager";
+import DiceCtrl from "./DiceRoll";
 
 @ccclass('PlayerControl')
 export class PlayerControl extends cc.Component {
     @property(cc.String)
     playerName: string = '';
-    @property(cc.String)
-    playerId: string = '';
+    @property(cc.Float)
+    playerId: number = 1;
     @property(cc.Float)
     playerAvatar: number = 0;
 
     @property(cc.Vec2)
     position: cc.Vec2 = cc.v2(0, 0);
 
-    rollDice(dice: number[]): number {
-        /*
-        * Rolls a die and returns the result.
-        * @param {number[]} dice - The array of dice values to roll.
-        * @returns {number} The rolled value from the provided dice array.
-        */
+    getPlayerInfo() {
+        const playerData: PlayerData = GameManager.getInstance().getPlayerData(this.playerId);
+        this.playerName = playerData.name;
+        this.playerAvatar = playerData.avatar;
+    }
 
-        if (dice.length === 0) {
-            console.warn("No dice provided for rolling.");
-            return 0;
-        }
-        const randomIndex = Math.floor(Math.random() * dice.length);
-        const rolledValue = dice[randomIndex];
-        console.log(`Rolled value: ${rolledValue} from dice: ${dice}`);
+    initializePosition() {
 
-        return rolledValue;
+    }
+
+    // Life-cycle callbacks
+    onLoad() {
+        this.getPlayerInfo();
+        this.initializePosition();
     }
 }
