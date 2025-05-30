0883cb085a3d3038e3d9f02b703bfd34f9a6511a
112062228 <venceching1015@gmail.com>
2025-05-30 17:59:14 +0800

    Map & Dice Problem Solved

--- assets/scripts/DiceRoll.ts
+++ assets/scripts/DiceRoll.ts
@@ -1,7 +1,7 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class DiceCtrl extends cc.Component {
+export default class DiceRoll extends cc.Component {
 
     @property(cc.Animation)
     diceAnimation: cc.Animation = null;
