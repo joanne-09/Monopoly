93ef670186cda0acb09ece3d4f33ae8a9c3a09e9
BenjaMine0411 <ericyu7777.us@gmail.com>
2025-06-04 11:05:05 +0800

    建立多人連線，但依舊有諸多問題

--- assets/scripts/types/PhotonEventCodes.ts
+++ assets/scripts/types/PhotonEventCodes.ts
@@ -2,4 +2,6 @@ export enum PhotonEventCodes {
     SEND_MESSAGE = 1,
     PLAYER_MOVEMENT = 2,
     PLAYER_JOINED = 3,
+    PLAYER_THROW_ACTION = 4, // Added
+    PLAYER_HIT_ACTION = 5,   // Added
 }
